6e7fc70df21f848c8a618bd3d56ce6234ed4b3ee
Christian Meunier <chris@jumpn.com>
2018-06-26 16:31:35 +0800

    Update Postgrex dependency

--- mix.exs
+++ mix.exs
@@ -4,28 +4,32 @@ defmodule EctoReplaySandbox.Mixfile do
   @version "1.0.0"
 
   def project do
-    [app: :ecto_replay_sandbox,
-     version: @version,
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     package: package(),
-     source_url: "https://github.com/jumpn/ecto_replay_sandbox",
-     docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
-     deps: deps()]
+    [
+      app: :ecto_replay_sandbox,
+      version: @version,
+      elixir: "~> 1.4",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      package: package(),
+      source_url: "https://github.com/jumpn/ecto_replay_sandbox",
+      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
+      deps: deps()
+    ]
   end
 
   defp package do
-    [description: "Log replay based sandbox for Ecto, compatible with CockroachDB",
-     files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Christian Meunier"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/jumpn/ecto_replay_sandbox"}]
+    [
+      description: "Log replay based sandbox for Ecto, compatible with CockroachDB",
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Christian Meunier"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/jumpn/ecto_replay_sandbox"}
+    ]
   end
 
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger]
     ]
   end
 
@@ -34,8 +38,8 @@ defmodule EctoReplaySandbox.Mixfile do
       {:ex_doc, ">= 0.0.0", only: :dev},
       {:ecto, "~> 2.2"},
       {:db_connection, "~> 1.1"},
-      #{:postgrex, "~> 0.13"},
-      {:postgrex, "~> 0.13", hex: :postgrex_cdb, override: true},
+      # {:postgrex, "~> 0.13"},
+      {:postgrex, "~> 0.14.0-dev", hex: :postgrex_cdb, override: true}
     ]
   end
 end
